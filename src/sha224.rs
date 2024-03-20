19e8552a8d4d9ebb47c65e2779760fb7b6438717
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-03-20 12:55:35 -0400

    Rename `register_(hash)_functions` to plural for consistency

--- src/sha224.rs
+++ src/sha224.rs
@@ -11,16 +11,16 @@ use crate::rusqlite::{Connection, Result};
 ///
 /// ```
 /// # use sqlite_hashes::rusqlite::{Connection, Result};
-/// # use sqlite_hashes::register_sha224_function;
+/// # use sqlite_hashes::register_sha224_functions;
 /// # fn main() -> Result<()> {
 /// let db = Connection::open_in_memory()?;
-/// register_sha224_function(&db)?;
+/// register_sha224_functions(&db)?;
 /// let hash: Vec<u8> = db.query_row("SELECT sha224('hello')", [], |r| r.get(0))?;
 /// let expected = b"\xea\x09\xae\x9c\xc6\x76\x8c\x50\xfc\xee\x90\x3e\xd0\x54\x55\x6e\x5b\xfc\x83\x47\x90\x7f\x12\x59\x8a\xa2\x41\x93";
 /// assert_eq!(hash, expected);
 /// # Ok(())
 /// # }
 /// ```
-pub fn register_sha224_function(conn: &Connection) -> Result<()> {
+pub fn register_sha224_functions(conn: &Connection) -> Result<()> {
     crate::scalar::create_hash_fn::<Sha224>(conn, "sha224")
 }
