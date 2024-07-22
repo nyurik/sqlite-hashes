353ba34b085426480168c8a8995e516abb457dff
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-07-22 06:54:52 -0400

    Update deps, update extension registration

--- src/cdylib/cdylib.rs
+++ src/cdylib/cdylib.rs
@@ -1,36 +1,21 @@
-// See https://github.com/rusqlite/rusqlite/pull/1425
-macro_rules! sqlite3_extension_init {
-    ($func: ident) => {
-        #[allow(clippy::not_unsafe_ptr_arg_deref)]
-        #[no_mangle]
-        pub extern "C" fn sqlite3_extension_init(
-            db: *mut ::rusqlite::ffi::sqlite3,
-            pz_err_msg: *mut *mut std::os::raw::c_char,
-            p_api: *mut ::rusqlite::ffi::sqlite3_api_routines,
-        ) -> std::os::raw::c_int {
-            if p_api.is_null() {
-                return ::rusqlite::ffi::SQLITE_ERROR;
-            }
-
-            let res = unsafe { ::rusqlite::Connection::extension_init2(db, p_api) }.and_then($func);
-
-            if let Err(err) = res {
-                return unsafe { ::rusqlite::to_sqlite_error(&err, pz_err_msg) };
-            }
-
-            ::rusqlite::ffi::SQLITE_OK
-        }
-    };
-}
+use std::os::raw::{c_char, c_int};
 
 use rusqlite::ffi::SQLITE_NOTICE;
 use rusqlite::trace::log;
-use rusqlite::Connection;
+use rusqlite::{ffi, Connection, Result};
 
-sqlite3_extension_init!(init_loadable_extension);
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+#[no_mangle]
+pub unsafe extern "C" fn sqlite3_extension_init(
+    db: *mut ffi::sqlite3,
+    pz_err_msg: *mut *mut c_char,
+    p_api: *mut ffi::sqlite3_api_routines,
+) -> c_int {
+    Connection::extension_init2(db, pz_err_msg, p_api, extension_init)
+}
 
-fn init_loadable_extension(db: Connection) -> ::rusqlite::Result<()> {
+fn extension_init(db: Connection) -> Result<bool> {
     sqlite_hashes::register_hash_functions(&db)?;
     log(SQLITE_NOTICE, "Loaded sqlite_hashes extension");
-    Ok(())
+    Ok(false)
 }
